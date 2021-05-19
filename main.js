566c050b97303a78e866cd5a023906bdd3201509
Maina Kibe <66884954+Mainakibe@users.noreply.github.com>
2021-05-19 14:37:11 +0300

    added the toggle feature

---
+++ main.js
@@ -0,0 +1,21 @@
+const checkbox = document.getElementById('checkbox')
+const basicPlan = document.querySelector('.basic-plan')
+const proPlan = document.querySelector('.pro-plan')
+const masterPlan = document.querySelector('.master-plan')
+const label = document.querySelector('.label')
+
+checkbox.addEventListener('change',()=>{
+    if(checkbox.checked){
+        basicPlan.innerHTML=` 19.99`
+        proPlan.innerHTML=` 29.99`
+        masterPlan.innerHTML=` 39.99`
+    }else{
+        basicPlan.innerHTML=` 199.99`
+        proPlan.innerHTML=` 299.99`
+        masterPlan.innerHTML=` 399.99`
+    }
+})
+
+
+
+
